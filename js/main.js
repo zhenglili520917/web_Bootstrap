8bc75c84abb447c73e249aa37c6791b6cb61eca0
zhenglili520917 <zhenglili520917@gmail.com>
2019-09-19 20:17:38 +0800

    121

--- js/main.js
+++ js/main.js
@@ -3,10 +3,30 @@
 // animate 動畫 fadeIn
 // scrollTop 元素的上方
 // jqReady
-$(document).$(document).ready(function () {
+$(document).ready(function () {
     // jqClick
     $('#back-to-top').click(function (e) { 
         // 找到 html, body 執行動畫 ({動畫屬性: 值}, 時間);
         $('html, body').animate({scrollTop: 0}, 1000);
     });
+
+// 捲動 scroll()
+    $(window).scroll(function () {
+        // 輸出訊息至控制台
+        // console.log('捲動中!');
+        // 告知出現位置
+        // console.log($(window).scrollTop());
+
+        // 如果 螢幕高度 >=300 就顯示按鈕
+        // 否則 就隱藏按鈕
+        if ($(window).scrollTop() >=300) {
+            
+            $('#back-to-top').fadeIn(500);
+        } else {
+
+            $('#back-to-top').fadeOut(500);
+        }
+    });
+
+
 });
